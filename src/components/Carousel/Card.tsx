430f33fe96d56f9340379f1901a3d358d000b8e3
Audrey Aubouin <ca.haestie@gmail.com>
2024-08-17 13:21:31 +0200

    clean console.log

--- src/components/Carousel/Card.tsx
+++ src/components/Carousel/Card.tsx
@@ -21,12 +21,12 @@ function Card({data, cardRef, transitionFinished, trasnsType, transX, width, gap
     
     }, [currentTvShow])
     if(transitionFinished){
-        return ( <div className="card-app" onClick={() => {updateCurrentTvShow(data)}} style={{ background: `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0)), url("${BACKDROP_BASE_SMALL_URL}${data.backdrop_path}") no-repeat center / cover`, width:`${!result.matches ?  `${width}px`: `calc(100vw - 40px)`}`, height:`${height}px`, marginRight:`${!result.matches ? gap : 10}px`, transition: `${trasnsType}`, marginLeft:`${result.matches ? gap * 0.65 : 0}px`, transform: `translateX(${transX}px)`}} ref={cardRef}>
+        return ( <div className="card-app" onClick={() => {updateCurrentTvShow(data)}} style={{ background: `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0)), url("${BACKDROP_BASE_SMALL_URL}${data.backdrop_path}") no-repeat center / cover`, width:`${!result.matches ?  `${width}px`: `calc(100vw - 40px)`}`, height:`${height}px`, marginRight:`${!result.matches ? gap : 10}px`, transition: `${trasnsType}`, marginLeft:`${result.matches ? gap * 0.5 : 0}px`, transform: `translateX(${transX}px)`}} ref={cardRef}>
             <span className='title'>{data.name}</span>
         </div> )
     }
     else {
-        return ( <div onClick={() => { updateCurrentTvShow(data); }}  className="card-app" ref={cardRef} style={{ background: `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0)), url("${BACKDROP_BASE_SMALL_URL}${data.backdrop_path}") no-repeat center / cover`, width:`${!result.matches ? `${width}px`: `calc(100vw - 40px)`}`, height:`${height}px`, marginRight:`${!result.matches ? gap :  10}px`, marginLeft:`${result.matches ? gap * 0.65 : 0}px`}}>
+        return ( <div onClick={() => { updateCurrentTvShow(data); }}  className="card-app" ref={cardRef} style={{ background: `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0)), url("${BACKDROP_BASE_SMALL_URL}${data.backdrop_path}") no-repeat center / cover`, width:`${!result.matches ? `${width}px`: `calc(100vw - 40px)`}`, height:`${height}px`, marginRight:`${!result.matches ? gap :  10}px`, marginLeft:`${result.matches ? gap * 0.5 : 0}px`}}>
             <span className='title'>{data.name}</span>
         </div> )
     }
