04722db46ca32737ccb55c23b234c30a85fe223e
Audrey Aubouin <ca.haestie@gmail.com>
2024-08-17 23:10:46 +0200

    test mep

--- src/config.tsx
+++ src/config.tsx
@@ -2,6 +2,6 @@
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY_PARAM = "?api_key=ccc2cb0aa2507aa9a31ab3cf556e1260";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
-const BACKDROP_BASE_SMALL_URL = "https://image.tmdb.org/t/p/w300";
+const BACKDROP_BASE_SMALL_URL = "https://image.tmdb.org/t/p/w800";
 
 export {BASE_URL, API_KEY_PARAM, BACKDROP_BASE_SMALL_URL, BACKDROP_BASE_URL};
